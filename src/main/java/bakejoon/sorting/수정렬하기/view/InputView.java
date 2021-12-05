b9b3e2c1f0ee9eb1a919845623e57cda4d3bbdf7
Yunkeun <yunx2@naver.com>
2021-12-05 21:11:48 +0900

    feat: 둘째 줄부터 N개의 줄에 숫자 입력

--- src/main/java/bakejoon/sorting/수정렬하기/view/InputView.java
+++ src/main/java/bakejoon/sorting/수정렬하기/view/InputView.java
@@ -1,13 +1,31 @@
 package bakejoon.sorting.수정렬하기.view;
 
-import bakejoon.sorting.수정렬하기.model.Number;
+import bakejoon.sorting.수정렬하기.model.NumberSize;
+import bakejoon.sorting.수정렬하기.model.Numbers;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
 
-	public static Number getNumber(Scanner scanner) {
+	private final static int MINIMUM_INDEX_LOOP = 0;
+
+	private InputView() {
+	}
+
+	public static NumberSize getNumber(Scanner scanner) {
 		int number = Integer.parseInt(getInput(scanner));
-		return new Number(number);
+		return new NumberSize(number);
+	}
+
+	public static Numbers getNumbers(Scanner scanner, NumberSize number) {
+		List<Integer> numbers;
+
+		numbers = new ArrayList<>();
+		for (int i = MINIMUM_INDEX_LOOP; i < number.getNum(); i++) {
+			numbers.add(Integer.parseInt(getInput(scanner)));
+		}
+		return new Numbers(numbers);
 	}
 
 	public static String getInput(Scanner scanner) {
