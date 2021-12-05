9dee3bca3d215acea5904649a853efa102c57c38
Yunkeun <yunx2@naver.com>
2021-12-05 21:13:57 +0900

    feat: 입력한 N개의 숫자를 오름차순 정렬

--- src/main/java/bakejoon/sorting/수정렬하기/controller/NumberController.java
+++ src/main/java/bakejoon/sorting/수정렬하기/controller/NumberController.java
@@ -1,12 +1,18 @@
 package bakejoon.sorting.수정렬하기.controller;
 
-import bakejoon.sorting.수정렬하기.model.Number;
+import bakejoon.sorting.수정렬하기.model.NumberSize;
+import bakejoon.sorting.수정렬하기.model.Numbers;
 import bakejoon.sorting.수정렬하기.view.InputView;
 import java.util.Scanner;
 
 public class NumberController {
 
+	private NumberController() {
+	}
+
 	public static void controlNumber(Scanner scanner) {
-		Number number = InputView.getNumber(scanner);
+		NumberSize numbersSize = InputView.getNumber(scanner);
+		Numbers numbers = InputView.getNumbers(scanner, numbersSize);
+		numbers.sortNumbers();
 	}
 }
