b44f27a202084b6cb03602430aeb3503fc726bd1
Yunkeun <yunx2@naver.com>
2021-12-06 18:53:32 +0900

    style: 필요없는 공백 제거

--- src/main/java/bakejoon/sorting/나이순정렬/view/OutputView.java
+++ src/main/java/bakejoon/sorting/나이순정렬/view/OutputView.java
@@ -2,8 +2,6 @@ package bakejoon.sorting.나이순정렬.view;
 
 public class OutputView {
 
-
-
 	public static void printMember(int age, String name) {
 		System.out.println(age + InputView.SPACE + name);
 	}
