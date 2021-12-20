a3e9c50bd2350aeb22c7bad3e6e7fb2859a67eea
Yunkeun <yunx2@naver.com>
2021-12-20 22:44:12 +0900

    feat: 자연수 S 입력

--- src/main/java/bakejoon/greedy/수들의합/view/InputView.java
+++ src/main/java/bakejoon/greedy/수들의합/view/InputView.java
@@ -1,5 +1,14 @@
 package bakejoon.greedy.수들의합.view;
 
+import java.util.Scanner;
+
 public class InputView {
 
+	private InputView() {
+	}
+
+	public static int inputSum(Scanner scanner) {
+		String inputNumber = scanner.nextLine();
+		return Integer.parseInt(inputNumber);
+	}
 }
